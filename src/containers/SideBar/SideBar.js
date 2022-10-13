230ef72620b72ae24841743ce2b241db932297de
anastasiiadmm <37694421+anastasiiadmm@users.noreply.github.com>
2022-10-14 00:38:35 +0600

    Added list menu with styles

--- src/containers/SideBar/SideBar.js
+++ src/containers/SideBar/SideBar.js
@@ -5,8 +5,15 @@ import menu_dots from "assets/images/menu_dots.png";
 const SideBar = ({children}) => {
   return (
     <div className="content">
-      <div className="content__menu">
-        <img className="content__menu-dots" src={menu_dots} alt="menu_dots" />
+      <div className="content__menu_list">
+        <div className="content__menu">
+          <img className="content__menu-dots" src={menu_dots} alt="menu_dots"/>
+          <ul className="content__submenu">
+            <li><a href="#">Проекты</a></li>
+            <li><a href="#">О компании</a></li>
+            <li><a href="#">Контакты</a></li>
+          </ul>
+        </div>
       </div>
       {children}
     </div>
