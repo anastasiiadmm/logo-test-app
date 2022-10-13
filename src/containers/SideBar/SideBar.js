fb48d73993b45b9b13eca8db00ee6b2a6d43d132
anastasiiadmm <37694421+anastasiiadmm@users.noreply.github.com>
2022-10-14 00:20:45 +0600

    Added img for menu dots

--- src/containers/SideBar/SideBar.js
+++ src/containers/SideBar/SideBar.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import "assets/scss/_content.scss";
+import menu_dots from "assets/images/menu_dots.png";
 
 const SideBar = ({children}) => {
   return (
     <div className="content">
-      <div className="content__menu">menu</div>
+      <div className="content__menu">
+        <img className="content__menu-dots" src={menu_dots} alt="menu_dots" />
+      </div>
       {children}
     </div>
   );
